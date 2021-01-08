816c45f8646d38949b6c78ea5dd86ec2eeba21e2
Ryan Hodges <ryan.d.hodges@gmail.com>
2021-01-07 17:30:07 -0800

    allow ol4_settings mapSettings to coexist between visualize and scenarios

--- scenarios/static/scenarios/js/ol4_settings.js
+++ scenarios/static/scenarios/js/ol4_settings.js
@@ -34,68 +34,73 @@
         }
     };
 
-    var mapSettings = {
-        getInitMap: function() {
-            // ol.Map.getLayers = function() {
-            //     return ol.Map.prototype.getLayers.call(this).getArray();
-            // };
-            // ol.Map.zoomToExtent = function(extent) {
-            //     ol.Map.prototype.getView.call(this).fit(extent, {duration: 1500});
-            // };
-            map = new ol.Map({
-                layers: [
-                    new ol.layer.Tile({
-                        source: new ol.source.OSM(),
-                        name: 'OSM Base Layer'
-                    }),
-                    // app.planningUnitsLayer,
-                    // app.scenariosLayer
-                ],
-                target: 'map',
-                view: new ol.View({
-                    center: [0, 0],
-                    zoom: 2
-                })
-            });
-            // map.addLayer = function(layer) {}
-            return map
-        },
-        configureLayer: function(layer) {
-            layer.addFeatures = function(features) {
-                for (var i=0; i < features.length; i++) {
-                    feature = features[i];
-                    layersource = layer.getSource();
-                    layersource.addFeature(feature);
-                }
-            }
-            layer.addWKTFeatures = function(wkt) {
-                var format = new ol.format.WKT();
-                geometry = format.readGeometry(wkt);
-                feature = format.readFeature(wkt, {
-                    // Re-write to take dataProjection in and determine featureProjection itself
-                    dataProjection: 'EPSG:3857',
-                    featureProjection: 'EPSG:3857'
-                });
-                layer.addFeatures([feature]);
-            }
-            layer.addGeoJSONFeatures = function(geojson_features) {
-                layer.addFeatures(new ol.format.GeoJSON().readFeatures(geojson_features));
-            }
-            layer.setVisibility = function(visibility) {
-                layer.setVisible(visibility);
-            }
-            layer.removeAllFeatures = function() {
-                var layersource = layer.getSource();
-                if (layersource) {
-                    layersource.clear();
-                }
-            }
-            layer.getDataExtent = function() {
-                return this.getSource().getExtent();
-            }
-            return layer;
-        }
+    // mapSettings is generally set in 'Visualize' - however scenarios needs a default value, and needs to append some functions as well.
+    if(mapSettings==undefined) {
+      var mapSettings = {
+          getInitMap: function() {
+              // ol.Map.getLayers = function() {
+              //     return ol.Map.prototype.getLayers.call(this).getArray();
+              // };
+              // ol.Map.zoomToExtent = function(extent) {
+              //     ol.Map.prototype.getView.call(this).fit(extent, {duration: 1500});
+              // };
+              map = new ol.Map({
+                  layers: [
+                      new ol.layer.Tile({
+                          source: new ol.source.OSM(),
+                          name: 'OSM Base Layer'
+                      }),
+                      // app.planningUnitsLayer,
+                      // app.scenariosLayer
+                  ],
+                  target: 'map',
+                  view: new ol.View({
+                      center: [0, 0],
+                      zoom: 2
+                  })
+              });
+              // map.addLayer = function(layer) {}
+              return map
+          },
+          configureLayer: function(layer) {
+              layer.addFeatures = function(features) {
+                  for (var i=0; i < features.length; i++) {
+                      feature = features[i];
+                      layersource = layer.getSource();
+                      layersource.addFeature(feature);
+                  }
+              }
+              layer.addWKTFeatures = function(wkt) {
+                  var format = new ol.format.WKT();
+                  geometry = format.readGeometry(wkt);
+                  feature = format.readFeature(wkt, {
+                      // Re-write to take dataProjection in and determine featureProjection itself
+                      dataProjection: 'EPSG:3857',
+                      featureProjection: 'EPSG:3857'
+                  });
+                  layer.addFeatures([feature]);
+              }
+              layer.addGeoJSONFeatures = function(geojson_features) {
+                  layer.addFeatures(new ol.format.GeoJSON().readFeatures(geojson_features));
+              }
+              layer.setVisibility = function(visibility) {
+                  layer.setVisible(visibility);
+              }
+              layer.removeAllFeatures = function() {
+                  var layersource = layer.getSource();
+                  if (layersource) {
+                      layersource.clear();
+                  }
+              }
+              layer.getDataExtent = function() {
+                  return this.getSource().getExtent();
+              }
+              return layer;
+          }
+      }
     }
+
+
 
     mapSettings.getInitFilterResultsLayer = function(layerName, style) {
     if (style) {
